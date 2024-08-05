1cc9ff799f704bbc02686b856eb297c1d91596fa
Samar Gupta <samargupta0710@gmail.com>
2024-08-06 00:34:25 +0530

    url updated

--- Backend/Frontend/src/context/SocketContext.jsx
+++ Backend/Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4002", {
+      const socket = io("https://real-time-chat-application-2fdn.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "https://real-time-chat-application-2fdn.onrender.com",
     methods: ["GET", "POST"],
   },
 });
