501a18c440214fc5168322f3354c8ad9c55ddb49
Samar Gupta <samargupta0710@gmail.com>
2024-08-05 23:53:19 +0530

    App Completed

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
@@ -28,6 +28,19 @@ try {
 //routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+
+
+// --------------------- code for deployment---------------
+if(process.env.NODE_ENV === "production"){
+    const dirPath = path.resolve();
+
+    app.use(express.static("./Frontend/dist"));
+    app.get("*",(req,res) =>{
+        res.sendFile(path.resolve(dirPath, "./Frontend/dist","index.html"));
+    })
+}
+
 
 server.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
